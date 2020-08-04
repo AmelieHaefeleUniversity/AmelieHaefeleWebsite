0de87b3533e53ca00a2c697f573986b025fd4fdf
Amelie Haefele <amelie@hafele.org>
2020-08-04 14:28:22 -0400

    Moved app to toplevel directory

--- application.py
+++ application.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 application = app = Flask(__name__)
+
 
 # https://stackoverflow.com/questions/43728500/python-flask-e-mail-form-example
 
-@app.route('/')
-def index():
-    return '<h1>Hello World</h1>'
+@app.route("/")
+def home():
+    return app.send_static_file("index.html")
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello %s!!</h1>' % name
+
+# @app.route('/user/<name>')
+# def user(name):
+# return '<h1>Hello %s!!</h1>' % name
 
 
 if __name__ == '__main__':
